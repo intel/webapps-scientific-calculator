d1de6b196f21bbae672f0811693013eb3a273f6b
Max Waterman <max.waterman@intel.com>
2014-02-26 16:58:02 +0000

    Adding crosswalk targets.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,119 @@ module.exports = function (grunt) {
     packageInfo: grunt.file.readJSON('package.json'),
     chromeInfo: grunt.file.readJSON('platforms/chrome-crx/manifest.json'),
 
+    crosswalk: {
+      arm: {
+        //"outDir": process.env.HOME+'/z/webapps/webapps-annex/build',
+        "outDir": 'build',
+
+        verbose: false,
+
+        // display name for the app on the device;
+        // the sanitisedName used to construct the Locations object later
+        // is derived from this
+        name: '<%= packageInfo.name %>',
+
+        // package for the app's generated Java files; this works best if
+        // you have at least one period character between two character
+        // strings, and no digits
+        pkg: 'org.org01.webapps.annex',
+
+        icon: 'icon_128.png',
+
+        fullscreen: true,
+
+        remoteDebugging: true,
+
+        // path to the directory containing your HTML5 app;
+        // note that this must use the correct path separators for your
+        // platform: Windows uses '\\' while Linux uses '/'
+        appRoot: 'build/xpk',
+
+        // relative path from appRoot of the entry HTML file for your app
+        appLocalPath: 'index.html',
+
+        // embed crosswalk itself into the package
+        //embedded: true,
+
+        // path to the root of your Android SDK installation;
+        // on Windows, use the path to the sdk directory inside
+        // the installation, e.g. 'c:\\android-sdk\\sdk'
+        // default: automatically obtain from the 'android' command's path
+        //androidSDKDir: '/opt/android-sdk-linux/',
+
+        // path to the xwalk_app_template directory; you can either
+        // download and unpack this manually, or use the xwalk_android_dl
+        // script to do so (part of this project; see the README for details);
+        // note that path separators specific to your platform must be used
+        // eg: export XWALK_APP_TEMPLATE=$HOME/Downloads/crosswalk-3.32.53.4-x86
+        //xwalkAndroidDir: project/specific/folder
+
+        // architecture of embedded crosswalk
+        // default: it is obtained from the contents of
+        // xwalkAndroidDir/native_libs/ if there is only one arch in there,
+        // else it should be specified as either 'x86' or 'arm'
+        arch: 'arm',
+
+        // default: automatically obtains latest from androidSDKDir/build-tools
+        //androidAPIVersion: "18.0.1"
+      },
+      x86: {
+        //"outDir": process.env.HOME+'/z/webapps/webapps-annex/build',
+        "outDir": 'build',
+
+        verbose: false,
+
+        // display name for the app on the device;
+        // the sanitisedName used to construct the Locations object later
+        // is derived from this
+        name: '<%= packageInfo.name %>',
+
+        // package for the app's generated Java files; this works best if
+        // you have at least one period character between two character
+        // strings, and no digits
+        pkg: 'org.org01.webapps.annex',
+
+        icon: 'icon_128.png',
+
+        fullscreen: true,
+
+        remoteDebugging: true,
+
+        // path to the directory containing your HTML5 app;
+        // note that this must use the correct path separators for your
+        // platform: Windows uses '\\' while Linux uses '/'
+        appRoot: 'build/xpk',
+
+        // relative path from appRoot of the entry HTML file for your app
+        appLocalPath: 'index.html',
+
+        // embed crosswalk itself into the package
+        //embedded: true,
+
+        // path to the root of your Android SDK installation;
+        // on Windows, use the path to the sdk directory inside
+        // the installation, e.g. 'c:\\android-sdk\\sdk'
+        // default: automatically obtain from the 'android' command's path
+        //androidSDKDir: '/opt/android-sdk-linux/',
+
+        // path to the xwalk_app_template directory; you can either
+        // download and unpack this manually, or use the xwalk_android_dl
+        // script to do so (part of this project; see the README for details);
+        // note that path separators specific to your platform must be used
+        // eg: export XWALK_APP_TEMPLATE=$HOME/Downloads/crosswalk-3.32.53.4-x86
+        //xwalkAndroidDir: project/specific/folder
+
+        // architecture of embedded crosswalk
+        // default: it is obtained from the contents of
+        // xwalkAndroidDir/native_libs/ if there is only one arch in there,
+        // else it should be specified as either 'x86' or 'arm'
+        arch: 'x86',
+
+        // default: automatically obtains latest from androidSDKDir/build-tools
+        //androidAPIVersion: "18.0.1"
+      }
+    },
+
     clean: ['build'],
 
     release: {
@@ -338,4 +451,9 @@ module.exports = function (grunt) {
   grunt.registerTask('sdk-install', ['sdk', 'install']);
 
   grunt.registerTask('default', 'wgt');
+  grunt.registerTask('apk', [
+    'xpk',
+    'crosswalk:x86',
+    'crosswalk:arm'
+  ]);
 };
--- HACKING.md
+++ HACKING.md
@@ -13,8 +13,8 @@ grunt-cli do:
 
 You should then install the client-side dependencies into app/lib/:
 
-    npm install -g bower
-    bower install
+  npm install -g bower
+  bower install
 
 Note that if you want to install the application to a Tizen device
 as a wgt file, you will also need to install the sdb tool first.
@@ -102,6 +102,27 @@ CSS, and HTML) using:
 
 Note that in both cases, the files comprising the packages are
 first copied into the build/wgt and build/sdk directories respectively.
+
+To create packages for Android use the 'apk' target:
+
+    grunt apk
+
+This will first build an 'xpk' target and then package two apks in
+build/ named AppName_{x86,arm}.apk.
+You can then install the appropriate one to your device as usual -
+for example, ```adb install -r build/AppName_x86.apk```.
+There are also targets to create packages just for a single architecture. They require the 'xpk' target to be build previously :
+
+    grunt xpk
+    grunt crosswalk:x86
+
+or :
+
+    grunt xpk
+    grunt crosswalk:arm
+
+Packaging for Android requires some set up - please see
+[crosswalk-apk-generator README.md](https://github.com/crosswalk-project/crosswalk-apk-generator/blob/master/README.md#pre-requisites).
 
 # GUIDE FOR MS WINDOWS USERS AND TIZEN IDE
 
